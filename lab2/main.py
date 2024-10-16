c181e4553ae3dd44b41cd4ba46d7ac84615d32c1
AleksandrZhukovin <sashokzuk@gmail.com>
2024-10-16 23:58:59 +0300

    add(lab): Add fully completed lab 2

---
+++ lab2/main.py
@@ -0,0 +1,26 @@
+"""
+Варіант 6
+z = 4*x1 + 2*x2 (max)
+{-x1 + 2*x2 <= 6
+{ x1 + x2 <= 9
+{ 3*x1 - x2 <= 15
+{ x1 >= 0, x2 >= 0
+"""
+
+from scipy.optimize import linprog
+
+
+coefs = [-4, -2]
+A = [
+    [-1, 2],
+    [1, 1],
+    [3, -1],
+]
+A0 = [6, 9, 15]
+
+res = linprog(coefs, A_ub=A, b_ub=A0,  bounds=[(0, None), (0, None)], method='simplex')
+x1 = res.x[0]
+x2 = res.x[1]
+print(f"x1 = {x1}")
+print(f"x2 = {x2}")
+print(f"z = {x1*4 + x2*2}")
